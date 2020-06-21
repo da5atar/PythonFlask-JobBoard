cc8a56f9c4ac817811c605af505f171048d420fe
Massamba Sow <mass.sow@gmail.com>
2020-06-21 20:18:17 +0000

    Add database connection

--- jobs/app.py
+++ jobs/app.py
@@ -1,8 +1,37 @@
+# TODO 3: Preparing for dynamic content
+import sqlite3
 # TODO 1: Flask setup
-from flask import Flask, render_template
+from flask import Flask, render_template, g
+
+PATH = "db/jobs.sqlite"
 
 app = Flask(__name__)
 
+# global database connection
+def open_connection():
+    connection = getattr(g, "_connection", None)
+    if connection == None:
+        connection = g._connection = sqlite3.connect(PATH)
+    connection.row_factory = sqlite3.Row
+    return connection
+
+# query database function
+def execute_sql(sql, values=(), commit=False, single=False):
+    connection = open_connection()
+    cursor = connection.execute(sql, values)
+    if commit:
+        results = connection.commit()
+    else:
+        results = cursor.fetchone() if single else cursor.fetchall()
+    cursor.close()
+    return results
+
+# close the connection
+@app.teardown_appcontext
+def close_connection(exception):
+    connection = getattr(g, "_connection", None)
+    if connection is not None:
+        connection.close()
 
 @app.route('/')
 @app.route("/jobs")

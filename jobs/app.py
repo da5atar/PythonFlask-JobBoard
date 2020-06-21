8e6863338c383d2f76d928d5ef7e9aca714f1681
Massamba Sow <mass.sow@gmail.com>
2020-06-21 20:49:35 +0000

    Add macro to display all jobs

--- jobs/app.py
+++ jobs/app.py
@@ -36,4 +36,11 @@ def close_connection(exception):
 @app.route('/')
 @app.route("/jobs")
 def jobs():
-    return render_template("index.html")
+    # TODO 4
+    jobs = execute_sql('SELECT job.id, '
+                       'job.title, '
+                       'job.description, '
+                       'job.salary, '
+                       'employer.id as employer_id, '
+                       'employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id')
+    return render_template("index.html", jobs=jobs)
